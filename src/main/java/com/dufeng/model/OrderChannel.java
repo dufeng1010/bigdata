d1837562f3c4d9dc8429e9c0e0818585faa6394c
yandufeng <yandufeng@readtv.cn>
2017-05-22 17:38:50 +0800

    close #17

---
+++ src/main/java/com/dufeng/model/OrderChannel.java
@@ -0,0 +1,20 @@
+package com.dufeng.model;
+
+public class OrderChannel {
+    
+    private String label;
+    private long count;
+    
+    public String getLabel() {
+        return label;
+    }
+    public long getCount() {
+        return count;
+    }
+    public void setLabel(String label) {
+        this.label = label;
+    }
+    public void setCount(long count) {
+        this.count = count;
+    }
+}
